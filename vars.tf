0535d8c13d0751dd7951dac5a2fc3933d93ddfd7
91996 <mukunca50@gmail.com>
2023-04-17 08:45:03 +0530

    vars.tf

--- vars.tf
+++ vars.tf
@@ -6,42 +6,43 @@ variable "instances" {
     }
     user = {
       name = "mongodb"
-      type = "t3.small"
+      type = "t3.micro"
     }
     user = {
       name = "catalogue"
-      type = "t3.small"
+      type = "t3.micro"
     }
     user = {
       name = "redis"
-      type = "t3.small"
+      type = "t3.micro"
     }
     user = {
       name = "cart"
-      type = "t3.small"
+      type = "t3.micro"
+
     }
     user = {
       name = "user"
-      type = "t3.small"
+      type = "t3.micro"
     }
     user = {
       name = "rabbitmq"
-      type = "t3.small"
+      type = "t3.micro"
       password="roboshop123"
     }
     user = {
       name = "mysql"
-      type = "t3.small"
+      type = "t3.micro"
       password="Roboshop@1"
     }
     user = {
       name = "shipping"
-      type = "t3.small"
+      type = "t3.micro"
       password="Roboshop@1"
     }
     user = {
       name = "payment"
-      type = "t3.small"
+      type = "t3.micro"
       paasword="roboshop123"
     }
   }
