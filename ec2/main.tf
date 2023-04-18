2896bc64fcf39227e3e925446686293f8ca13d08
91996 <mukunca50@gmail.com>
2023-04-18 14:30:23 +0530

    main.yml

--- ec2/main.tf
+++ ec2/main.tf
@@ -40,14 +40,13 @@ resource "aws_security_group" "sg" {
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-  tags = {
+ tags = {
     Name = "${var.component}-${var.env}-sg"
   }
 
