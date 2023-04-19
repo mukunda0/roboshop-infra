e1c7e8b03471b1152872e39abcd4fc9ab78912e0
91996 <mukunda50@gmail.com>
2023-04-20 00:43:38 +0530

    main.tfvars

--- ec2/main.tf
+++ ec2/main.tf
@@ -12,18 +12,20 @@ resource "aws_instance" "ec2" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags                   = {
     Name = var.component
-
-  }}
- resource  "provisioner" "remote-exec" {
-    connection {
-      host     = aws_instance.ec2.public_ip
-      user     = "centos"
-      password = "DevOps321"
-    }
-    inline = [
-      "ansible-pull -i localhost, -U https://github.com/mukunda0/roboshop-ansible.git roboshop.yml -e role_name=${var.component}"
-    ]
   }
+  }
+ resource "null_resource" "provisioner" {
+   provisioner "remote-exec" {
+     connection {
+       host     = aws_instance.ec2.public_ip
+       user     = "centos"
+       password = "DevOps321"
+     }
+     inline = [
+       "ansible-pull -i localhost, -U https://github.com/mukunda0/roboshop-ansible.git roboshop.yml -e role_name=${var.component}"
+     ]
+   }
+ }
 
 
 resource "aws_security_group" "sg" {
