c43079a7f9ed607f578a003c3016ff4a247ec955
91996 <mukunda50@gmail.com>
2023-04-21 21:04:03 +0530

    main.tf

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -7,6 +7,12 @@ resource "aws_spot_instance_request" "node1" {
     name ="prometheus-test-server"
   }
 }
+
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prometheus-test-server"
+}
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-064eaf18e03a4bb5d"
   instance_type = "t3.micro"
@@ -14,4 +20,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     name ="prometheus-test-node"
   }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prometheus-test-node"
 }
